275b3b5abe55123f9876efd99c551a8c8e2ceb7f
Franz Nico Testado <ftestado@exist.com>
2023-05-16 15:40:42 +0800

    Initialize project

---
+++ src/main/java/com/jim/App.java
@@ -0,0 +1,13 @@
+package com.jim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
